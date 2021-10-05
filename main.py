cba152e2bf4142a90dd1647c646cbbe53413829d
Michael Brewer <mb166@students.uwf.edu>
2021-10-05 13:17:03 -0500

    Reworked to use a 1d array

--- main.py
+++ main.py
@@ -3,23 +3,18 @@
 #
 
 import random
+import node
 
 ##### Functions
 def randomizePuzzle():
-    listOfNumbers = [1,2,3,4,5,6,7,8,0]
-    random.shuffle(listOfNumbers)
-    listCounter = 0
-    for i in range(3):
-        for x in range(3):
-            puzzle[i][x] = listOfNumbers[listCounter]
-            listCounter += 1
+    random.shuffle(puzzle)
 
 def printPuzzle():
-    for i in range(3):
-        for x in range(3):
-            print(str(puzzle[i][x]), end = ' ')
-        else:
-            print(' ', end = '\n')   
+    for i in range(9):
+        print(str(puzzle[i]), end = ' ')
+        if(((i+1)%3) == 0):
+            print(" ", end='\n')
+      
 
 def goalCheck():
     if puzzle == goalState:
@@ -27,14 +22,28 @@ def goalCheck():
     else:
         return False
 
-#### "Main"
-puzzle = [[1,2,3],
-          [4,5,6],
-          [7,8,0]]
+def moveToEmpty(boardState, emptyIndex, toMoveIndex):
+    temp = boardState[toMoveIndex]
+    boardState[toMoveIndex] = emptyIndex
+    boardState[emptyIndex] = temp
 
-goalState = [[1,2,3],
-            [4,5,6],
-            [7,8,0]]
+#### "Main"
+puzzle = [1,2,3,4,5,6,7,8,0]
+
+goalState = [1,2,3,4,5,6,7,8,0]
+
+#has all adjacent indices
+neighbors = {
+    0 : [1,3],
+    1 : [0,2,4],
+    2 : [1,5],
+    3 : [4,0,6],
+    4 : [3,5,1,7],
+    5 : [4,2,8],
+    6 : [7,3],
+    7 : [6,8,4],
+    8 : [7,5],
+}
 
 print(goalCheck())
 
@@ -42,3 +51,5 @@ randomizePuzzle()
 
 printPuzzle()
 print(goalCheck())
+
+node1 = node.Node(puzzle, 0, goalState)
