6a34305f4fdbb721e0ee55987bb3637950d70022
Michael Brewer <mb166@students.uwf.edu>
2021-10-05 19:30:43 -0500

    Manhattan distance working but stuck in infinite loop instead of solving

--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 
 import random
 import node
+import copy
 
 ##### Functions
+solved = False
+
 def randomizePuzzle():
     random.shuffle(puzzle)
 
@@ -22,15 +25,99 @@ def goalCheck():
     else:
         return False
 
+def findEmpty(boardState):
+    indexCounter = 0
+    for current in boardState:
+        if current == 0:
+            return indexCounter
+        else:
+            indexCounter += 1
+
 def moveToEmpty(boardState, emptyIndex, toMoveIndex):
     temp = boardState[toMoveIndex]
-    boardState[toMoveIndex] = emptyIndex
+    boardState[toMoveIndex] = boardState[emptyIndex]
     boardState[emptyIndex] = temp
+
+def getManhattanCost(boardState):
+    cost = 0
+    indexCounter = 0
+    for current in boardState:
+        if current == 0:
+            indexCounter += 1
+            continue
+        
+        current = current - 1
+        gx = current % 3
+        gy = current // 3
+
+        x = indexCounter % 3
+        y = indexCounter // 3
+
+        tempCost = (abs(x - gx) + abs(y - gy))
+        cost += tempCost
+        #print((current+1)," ", tempCost)
+        indexCounter += 1
+
+    return cost
+
+def nextMove(currentNode):
+    if currentNode.state == goalState:
+        solved = True
+        return
+
+    emptySpace = findEmpty(currentNode.state)
+    emptyNeighbors = neighbors[emptySpace]
+
+    for neighbor in emptyNeighbors:
+        newBoardState = copy.deepcopy(currentNode.state)
+        #print("before swap: ", newBoardState)
+        moveToEmpty(newBoardState, emptySpace, neighbor)
+        #print("after swap: ", newBoardState)
+        newNode = node.Node(newBoardState, (currentNode.depth+1), currentNode, getManhattanCost(newBoardState))
+        if newNode.state == goalState:
+            print("solved")
+            newNode.printThis()
+            solved = True
+            return
+        #print(isExplored(newNode.state) == False)
+        if (isExplored(newNode.state) == False):
+            #newNode.printThis()
+            unexploredNodes.append(newNode)
+    #print("completed Move")
+
+def findNextMove():
+    nextNode = None
+    nextNodeCost = 10000
+    nodeIndexCounter = 0
+    nextNodeIndex = 0
+    for node in unexploredNodes:
+        tempCost = node.cost + node.depth
+        if tempCost < nextNodeCost:
+            nextNode = node
+            nextNodeCost = node.cost + node.depth
+            nextNodeIndex = nodeIndexCounter
+        nodeIndexCounter += 1
+    unexploredNodes.pop(nextNodeIndex)
+    exploredNodes.append(nextNode)
+    #print("found enxt move")
+    return nextNode
+
+def isExplored(boardState):
+    for current in exploredNodes:
+        if current.state == boardState:
+            return True
+
+    return False
+
+
 
 #### "Main"
 puzzle = [1,2,3,4,5,6,7,8,0]
 
 goalState = [1,2,3,4,5,6,7,8,0]
+
+unexploredNodes = []
+exploredNodes = []
 
 #has all adjacent indices
 neighbors = {
@@ -44,12 +131,24 @@ neighbors = {
     7 : [6,8,4],
     8 : [7,5],
 }
+#randomizePuzzle()
+#puzzle = [1,5,0,3,2,8,4,6,7]
+puzzle = [1,2,3,4,5,6,7,0,8]
+print(getManhattanCost(puzzle))
+startNode = node.Node(puzzle, 0, None, getManhattanCost(puzzle))
+#nextMove(startNode)
+startNode.printThis()
+print(startNode.cost + startNode.depth)
+print("---starter^-----")
+unexploredNodes.append(startNode)
+moveCount = 0
+#print(getManhattanCost(puzzle))
 
-print(goalCheck())
 
-randomizePuzzle()
-
-printPuzzle()
-print(goalCheck())
-
-node1 = node.Node(puzzle, 0, goalState)
+while(moveCount < 2000):
+    #print(len(exploredNodes))
+    nextNode = findNextMove()
+    #print(nextNode.state == goalState)
+    nextMove(nextNode)
+    moveCount += 1
+#print(moveCount)
--- node.py
+++ node.py
@@ -1,5 +1,13 @@
 class Node:
-    def __init__(self, state, depth, parent):
+    def __init__(self, state, depth, parent, cost):
         self.state = state
         self.depth = depth
         self.parent = parent
+        self.cost = cost
+
+    def printThis(self):
+        for i in range(9):
+            print(str(self.state[i]), end = ' ')
+            if(((i+1)%3) == 0):
+                print(" ", end='\n')
+        print("-----------")
